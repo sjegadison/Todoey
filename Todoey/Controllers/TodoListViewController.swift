fe8041bbfe6f7cb9f25f5e334fb1b7fe7d43cd40
Sivaguru Jegadison <sivaguru.jegadison@gmail.com>
2019-02-27 12:44:33 +0530

    Data persistence using NSCoder Complete

--- Todoey/Controllers/TodoListViewController.swift
+++ Todoey/Controllers/TodoListViewController.swift
@@ -15,27 +15,31 @@ class TodoListViewController: UITableViewController {
 //                                "Second","Second","Second","Second","Second","Second","Second","Third"]
     
     var itemArray = [Item]()
+    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
     
-    let defaults = UserDefaults.standard
+//    let defaults = UserDefaults.standard
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        if let items = defaults.array(forKey: "TodoListArray") as? [Item] {
-            itemArray = items
-        }
+        print(dataFilePath!)
+        loadItems()
+//        if let items = defaults.array(forKey: "TodoListArray") as? [Item] {
+//            itemArray = items
+//        }
         
-        var newItem = Item()
-        newItem.title = "First Item"
-        itemArray.append(newItem)
         
-        newItem = Item()
-        newItem.title = "Second Item"
-        itemArray.append(newItem)
-        
-        newItem = Item()
-        newItem.title = "Third Item"
-        itemArray.append(newItem)
+//        var newItem = Item()
+//        newItem.title = "First Item"
+//        itemArray.append(newItem)
+//
+//        newItem = Item()
+//        newItem.title = "Second Item"
+//        itemArray.append(newItem)
+//
+//        newItem = Item()
+//        newItem.title = "Third Item"
+//        itemArray.append(newItem)
 
     }
     
@@ -64,6 +68,7 @@ class TodoListViewController: UITableViewController {
 //        }
         
         itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+        saveItems()
         
         tableView.deselectRow(at: indexPath, animated: true)
         tableView.reloadData()
@@ -83,8 +88,13 @@ class TodoListViewController: UITableViewController {
             let newItem = Item()
             newItem.title = textField.text!
             self.itemArray.append(newItem)
-            self.defaults.set(self.itemArray, forKey: "TodoListArray")
+            self.saveItems()
+
+//            self.defaults.set(self.itemArray, forKey: "TodoListArray")
+            
             self.tableView.reloadData()
+            
+
         }
         
         alert.addTextField { (alertTextField) in
@@ -93,6 +103,31 @@ class TodoListViewController: UITableViewController {
         }
         alert.addAction(action)
         present(alert, animated: true, completion: nil)
+    }
+    
+    //MARK - Model Manipulation Methods
+    func saveItems() {
+        let encoder = PropertyListEncoder()
+        
+        do {
+            let data = try encoder.encode(itemArray)
+            try data.write(to: dataFilePath!)
+        } catch {
+            print("Error encoding Item Array, \(error)")
+        }
+    }
+    
+    func loadItems() {
+
+            if  let data = try? Data(contentsOf: dataFilePath!) {
+                let decoder = PropertyListDecoder()
+                do {
+                    itemArray = try decoder.decode([Item].self, from: data)
+                } catch {
+                    print("Error decoding Item Array, \(error)")
+                }
+               
+            }
     }
     
 }
